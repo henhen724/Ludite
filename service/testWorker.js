988e805c7c482e0eda7bee9f670fea9148fa2ad6
Henry Hunt <henhen7.24@gmail.com>
2019-11-26 16:56:03 -0600

    Worker work

--- service/testWorker.js
+++ service/testWorker.js
@@ -1,11 +1,12 @@
 const ipc = require('node-ipc');
 
 ipc.config.id = 'a-unique-process-name2';
+ipc.config.socketRoot = __dirname + '\\';
 ipc.config.retry = 1500;
 ipc.config.logger = console.log.bind(console);
 ipc.connectTo('a-unique-process-name1', () => {
     console.log("Made it here");
-    // ipc.of['a-unique-process-name1'].on('connect', () => {
-    //     ipc.of['a-unique-process-name1'].emit('a-unique-message-name', "The message we send");
-    // });
+    ipc.of['a-unique-process-name1'].on('connect', () => {
+        ipc.of['a-unique-process-name1'].emit('a-unique-message-name', "The message we send");
+    });
 });
--- service/worker.js
+++ service/worker.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const ipc = require('node-ipc');
 
 ipc.config.id = 'a-unique-process-name1';
+ipc.config.socketRoot = __dirname + '\\';
 ipc.config.retry = 1500;
 ipc.config.logger = console.log.bind(console);
 ipc.serve(() => ipc.server.on('a-unique-message-name', message => {
@@ -14,17 +15,11 @@ ipc.server.start();
 //PACKET SCRAPER
 //USE NODEMAILER TO SEND EMAIL
 
-ipc.server.start();
+findWindows = async () => {
+  console.log("FIND WINDOW");
+  window = await activeWin();
+  console.log("Window", window);
+  setTimeout(findWindows, 100);
+};
 
-
-// findWindows = async () => {
-//   console.log("FIND WINDOW");
-//   window = await activeWin();
-//   process.send("Window", window);
-// };
-// console.log("I'm starting my main loop");
-// for (var i = 0; i < 10; i++) {
-//   findWindows().catch(err => console.log(err));
-// }
-
-while (true);
+findWindows();
